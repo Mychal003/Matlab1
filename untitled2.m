5b3c732145b49071a3f01d7c8a7a5a03f19b833a
Michał Pawlik <pawlikm11@gmail.com>
2024-10-05 23:34:31 +0200

    Initial project version

---
+++ untitled2.m
@@ -0,0 +1,16 @@
+%% Generacj liczb pseudolosowych - pierwszy program
+% Generacja liczb pseudolosowych o rozkładzie narmalnym i rysowanie histogramu
+clear all; close all; clc
+
+%% Generacja liczb pseudolosowych
+
+N = 1000
+x = randn(N,1);
+%% Histofram
+
+histogram(x,50)                 % k = 50 - liczba przedziałów histogramu
+
+
+
+
+
